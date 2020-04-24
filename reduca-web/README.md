2c15fc2dfa7e5e05ceb010c8ecaabe2485c9ef74
Mcat <mcat95@gmail.com>
2020-04-24 22:57:09 +0200

    Connect to cloud firestore

--- reduca-web/README.md
+++ reduca-web/README.md
@@ -1,7 +1,10 @@
 # REDUCA WEB
 
-# Build steps
+## Build steps
 1. Clone repository
 2. npm install
 3. npm run build
 4. npm run deploy
+
+## Local development
+1. npm start
--- reduca-web/src/App.js
+++ reduca-web/src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {
+    useState,
+    useEffect,
+} from 'react';
 import logo from './logo.svg';
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+    const [ user, setUser ] = useState();
+    useEffect(() => {
+	const firebaseConfig = {
+	    apiKey: "AIzaSyBy7thcdptlNFSW0IqfJ7OI6SBtmpkZ_OU",
+	    authDomain: "euvsvirus-d0165.firebaseapp.com",
+	    databaseURL: "https://euvsvirus-d0165.firebaseio.com",
+	    projectId: "euvsvirus-d0165",
+	    storageBucket: "euvsvirus-d0165.appspot.com",
+	    messagingSenderId: "658551436420",
+	    appId: "1:658551436420:web:9c2aa64a04253e1824bed1"
+	};
+
+	firebase.initializeApp(firebaseConfig);
+	const db = firebase.firestore();
+	db.collection("users").doc("PUp3fvntRc0TPsjEjSov").onSnapshot(u => u && setUser(u.data()));
+    }, []);
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	    Reduca {JSON.stringify(user)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
